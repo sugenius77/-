558b6f6756159908588df974580ffca5e6681a88
nyh <dkzndk1100@naver.com>
2021-12-29 23:29:47 +0900

    config : DB 연결 - ORM 초기 구현

--- server/config/config.py
+++ server/config/config.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 import os
 
-# dotenv 사용
-load_dotenv
+load_dotenv()
+# dotenv 사용 
+
 DB_CONNECT = os.environ.get("DB_CONNECT")
+# local .env 파일 사용
+
 SQLALCHEMY_DATABASE_URI = DB_CONNECT
 SQLALCHEMY_TRACK_MODIFICATIONS = False
