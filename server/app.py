3266332e106aaaf55bc111dbaa92b6b55bc482ba
nyh <dkzndk1100@naver.com>
2021-12-29 23:29:47 +0900

    config : DB 연결 - ORM 초기 구현

--- server/app.py
+++ server/app.py
@@ -1,31 +1,21 @@
 from flask import Flask
+from models.db_connect import db
 from flask_migrate import Migrate
-from models.db_coonect import db
-from flask_cors import CORS
 from config import config
 from views import main_view
 
 def create_app():
+
     app = Flask(__name__)
-    
 
-    app.config.from_object(config) 
-    # config에서 가져온 파일 사용하기
-
-    db.init_app(app) 
-    #SQLAlchemy 객체를 app객체와 이어줌.
-    
+    app.config.from_object(config) # config에서 가져온 파일 사용하기
+    db.init_app(app) #SQLAlchemy 객체를 app객체와 이어줌.
     migrate = Migrate()
-    migrate.init_app(app,db)
-    # db 마이그레이션
-
-    CORS(app)
-
+    migrate.init_app(app, db)
+    
     app.register_blueprint(main_view.bp)
-    # view blueprint 받아오기
+    
 
     return app
-
-
-if __name__ =="__main__":
+if __name__ == '__main__':
     create_app().run(debug=True)
