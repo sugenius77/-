397c72de91c6efcb49d3d4bd9f9b359baceec967
nyh <dkzndk1100@naver.com>
2021-12-31 18:38:32 +0900

    feat : DB 테이블 분리

--- server/models/rabbitMenu.py
+++ server/models/rabbitMenu.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class rabbitMenu(db.Model):
     __tablename__ ='rabbitMenu'
-    __table_args__ = {'extend_existing': True}
+
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     menu_name = db.Column(db.String(20), nullable=False)
     image_url = db.Column(db.String(255), nullable=False)
--- server/models/rabbitTimeRanking.py
+++ server/models/rabbitTimeRanking.py
@@ -1,10 +1,10 @@
 from app import db
 from datetime import datetime
-
+    
 class rabbitTimeRanking(db.Model):
     __tablename__ ='rabbitTimeRanking'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     kinds_id = db.Column(db.Integer, db.ForeignKey('rabbitKinds.id'), nullable=False)
-    time_id = db.Column(db.Integer, db.ForeignKey('rabbitTime.id'), nullable=False)
+    time_id = db.Column(db.Integer, db.ForeignKey('rabbitDate.id'), nullable=False)
     time_rank =db.Column(db.Integer, nullable=True)
