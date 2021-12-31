53a77a16bb65d3aae1adeb3ca47fa36db92f8a9e
nyh <dkzndk1100@naver.com>
2021-12-31 18:38:32 +0900

    feat : DB 테이블 분리

---
+++ server/models/rabbitKinds.py
@@ -0,0 +1,8 @@
+from app import db
+from datetime import datetime
+
+class rabbitKinds(db.Model):
+    __tablename__ ='rabbitKinds'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    kinds_name = db.Column(db.String(10), nullable=False)
