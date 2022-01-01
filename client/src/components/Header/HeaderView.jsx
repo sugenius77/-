4601b46a831e2b977eb998158e0165620a745e32
Hongjoonhyung <sesa0802@naver.com>
2022-01-02 07:40:49 +0900

    feat:스크롤 이벤트로 투명도 조절하기

---
+++ client/src/components/Header/HeaderView.jsx
@@ -0,0 +1,40 @@
+import MainLogo from './MainLogo';
+import Menu from './Menu';
+import styled from 'styled-components';
+import { HeaderContext } from './Header';
+
+const Div = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #81c8ee;
+    padding: 8px 12px;
+    border-bottom: 2px solid black;
+    font-size: 24px;
+    background-color: white;
+
+    @media screen and (max-width: 1040px) {
+        flex-direction: column;
+        align-items: flex-start;
+    }
+`;
+
+const HeaderView = () => {
+    return (
+        <HeaderContext.Consumer>
+            {({ toggle, innerWidth }) => (
+                <Div>
+                    <MainLogo />
+                    {innerWidth >= 1040 && <Menu />}
+                    {innerWidth < 1040 && toggle && <Menu />}
+                </Div>
+            )}
+        </HeaderContext.Consumer>
+    );
+};
+
+export default HeaderView;
