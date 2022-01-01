4f1d64ed442abe3a41f4eb2152587933f59031c9
Hongjoonhyung <sesa0802@naver.com>
2022-01-02 08:46:20 +0900

    feat:스크롤 FadeOut 구현

--- client/src/components/Introduce/Introduce.jsx
+++ client/src/components/Introduce/Introduce.jsx
@@ -24,7 +24,7 @@ const Scene = styled.div`
 const MainMessage = styled.div`
     opacity: ${({ messageOpacity }) => messageOpacity};
     position: fixed;
-    top: 0;
+    top: 35vh;
     width: 100%;
     font-size: 2.5rem;
     font-weight: bold;
@@ -95,35 +95,6 @@ const Introduce = () => {
                         [0, 1, { start: 0.5, end: 0.6 }, { start: 0.65, end: 0.7 }],
                         [0, 1, { start: 0.7, end: 0.8 }, { start: 0.85, end: 0.9 }],
                     ],
-                    translateY: [
-                        [0, 20, { start: 0.1, end: 0.2 }, { start: 0.25, end: 0.3 }],
-                        [0, 20, { start: 0.3, end: 0.4 }, { start: 0.45, end: 0.5 }],
-                        [0, 20, { start: 0.5, end: 0.6 }, { start: 0.65, end: 0.7 }],
-                        [0, 20, { start: 0.7, end: 0.8 }, { start: 0.85, end: 0.9 }],
-                    ],
-                },
-            },
-            {
-                type: 'normal',
-                scrollHeight: heightNum * window.innerHeight,
-                heightNum: heightNum,
-                values: {
-                    opacity: [
-                        [0, 1, { start: 0.1, end: 0.2 }, { start: 0.25, end: 0.3 }],
-                        [0, 1, { start: 0.3, end: 0.4 }, { start: 0.35, end: 0.5 }],
-                        [0, 1, { start: 0.5, end: 0.6 }, { start: 0.65, end: 0.7 }],
-                        [0, 1, { start: 0.7, end: 0.8 }, { start: 0.85, end: 0.9 }],
-                    ],
-                    translateY: [
-                        [0, 20, { start: 0.1, end: 0.2 }, { start: 0.25, end: 0.3 }],
-                        [0, 20, { start: 0.3, end: 0.4 }, { start: 0.45, end: 0.5 }],
-                        [0, 20, { start: 0.5, end: 0.6 }, { start: 0.65, end: 0.7 }],
-                        [0, 20, { start: 0.7, end: 0.8 }, { start: 0.85, end: 0.9 }],
-=======
-                scrollHeight: heightNum * window.innerHeight,
-                heightNum: heightNum,
-                values: {
-                    messageOpacityA: [0, 1, { start: 0.1, end: 0.2 }],
                 },
             },
             {
@@ -208,11 +179,7 @@ const Introduce = () => {
                 heightNum: 1,
                 values: {
                     opacity: [
-<<<<<<< sprint
                         [0, 1, { start: 0.2, end: 0.25 }],
-=======
-                        [0, 1, { start: 0.1, end: 0.2 }],
->>>>>>> feat:스크롤 이벤트로 투명도 조절하기
                         [0, 1, { start: 0.3, end: 0.5 }],
                         [0, 1, { start: 0.6, end: 0.8 }],
                         [0, 1, { start: 0.85, end: 0.95 }],
@@ -265,23 +232,7 @@ const Introduce = () => {
             scrollHeight += sceneInfo[i].scrollHeight;
         }
         return scrollHeight;
-<<<<<<< sprint
     }, [currentScene, sceneInfo]);
-
-    //현재 보여지는 Scene 기준 스크롤 길이
-    const currentYoffset = useMemo(() => {
-        return yOffset - prevScrollHeight;
-    }, [yOffset, prevScrollHeight]);
-
-    useEffect(() => {
-        console.log(currentScene);
-    }, [currentScene]);
-    // 현재 스크롤의 비율에 따라 투명도 계산
-    const calcOpacity = useCallback(
-        (values) => {
-            let rv;
-            const scrollHeight = sceneInfo[currentScene].scrollHeight;
-            const scrollRatio = currentYoffset / sceneInfo[currentScene].scrollHeight;
 
             if (values.length === 4) {
                 const fadeInScrollStart = values[2].start * scrollHeight;
@@ -318,91 +269,55 @@ const Introduce = () => {
     const currentYoffset = useMemo(() => {
         console.log(yOffset);
         return yOffset - prevScrollHeight;
-    }, [yOffset]);
+    }, [yOffset, prevScrollHeight]);
 
     // // 현재 스크롤의 비율에 따라 원하는 메세지의 투명도를 바꾸기 위한 값
 
-    const calcValues = useCallback(
->>>>>>> feat:스크롤 이벤트로 투명도 조절하기
+    const fadeIn = useCallback(
         (values) => {
             let rv;
             const scrollHeight = sceneInfo[currentScene].scrollHeight;
             const scrollRatio = currentYoffset / sceneInfo[currentScene].scrollHeight;
 
-<<<<<<< sprint
             if (values.length === 4) {
-                const translateInScrollStart = values[2].start * scrollHeight;
-                const translateInScrollEnd = values[2].end * scrollHeight;
-                const translateInScrollHeight = translateInScrollEnd - translateInScrollStart;
+                const fadeInScrollStart = values[2].start * scrollHeight;
+                const fadeInScrollEnd = values[2].end * scrollHeight;
+                const fadeInScrollHeight = fadeInScrollEnd - fadeInScrollStart;
 
-                const translateOutScrollStart = values[3].start * scrollHeight;
-                const translateOutScrollEnd = values[3].end * scrollHeight;
+                const fadeOutScrollStart = values[3].start * scrollHeight;
+                const fadeOutScrollEnd = values[3].end * scrollHeight;
+                const fadeOutScrollHeight = fadeInScrollEnd - fadeInScrollStart;
 
-                if (translateInScrollStart <= currentYoffset && currentYoffset <= translateInScrollEnd) {
-                    rv = -((currentYoffset - translateInScrollStart) / translateInScrollHeight) * (values[1] - values[0]);
-                } else if (translateInScrollEnd <= currentYoffset && currentYoffset <= translateOutScrollStart) {
-                    rv = values[1];
-                }
-                if (translateInScrollStart <= currentYoffset && currentYoffset <= translateOutScrollEnd) {
-                    rv = ((translateOutScrollEnd - currentYoffset) / translateInScrollHeight) * (values[1] - values[0]);
-                }
-=======
-            if (values.length === 3) {
-                const partScrollStart = values[2].start * scrollHeight;
-                const partScrollEnd = values[2].end * scrollHeight;
-                const partScrollHeight = partScrollEnd - partScrollStart;
-
-                if (partScrollStart <= currentYoffset && currentYoffset <= partScrollEnd) {
-                    rv = ((currentYoffset - partScrollStart) / partScrollHeight) * (values[1] - values[0]);
-                } else if (currentYoffset < partScrollStart) {
+                if (fadeInScrollStart <= currentYoffset && currentYoffset <= fadeInScrollEnd) {
+                    rv = ((currentYoffset - fadeInScrollStart) / fadeInScrollHeight) * (values[1] - values[0]);
+                } else if (currentYoffset < fadeInScrollStart) {
                     rv = values[0];
-                } else if (currentYoffset > partScrollEnd) {
-                    rv = values[1];
+                } else if (currentYoffset > fadeInScrollEnd) {
+                    rv = values[0];
                 }
->>>>>>> feat:스크롤 이벤트로 투명도 조절하기
+                if (fadeInScrollEnd <= currentYoffset && currentYoffset <= fadeOutScrollEnd) {
+                    rv = ((fadeOutScrollEnd - currentYoffset) / fadeOutScrollHeight) * (values[1] - values[0]);
+                }
             } else {
                 rv = scrollRatio * (values[1] - values[0]) + values[0];
             }
             return rv;
         },
 
-<<<<<<< sprint
         [currentYoffset, currentScene, sceneInfo],
     );
 
     const messageOpacity = useMemo(() => {
         const message = [];
+
+        const currentRatio = currentYoffset - prevScrollHeight;
         let values = sceneInfo[currentScene].values.opacity;
 
         for (let i = 0; i < values.length; i++) {
-            message.push(calcOpacity(values[i]));
+            message.push(fadeIn(values[i]));
         }
         return message;
-    }, [currentScene, sceneInfo, calcOpacity]);
-
-    const messageTranslateY = useMemo(() => {
-        const message = [];
-        let values = sceneInfo[currentScene].values.translateY;
-
-        for (let i = 0; i < values.length; i++) {
-            message.push(calcTranslateY(values[i]));
-        }
-        return message;
-    }, [sceneInfo, currentScene, calcTranslateY]);
-
-    return (
-        <ScrollContext.Provider value={{ currentScene, sceneInfo, messageOpacity, messageTranslateY }}>
-            <IntroduceView />
-        </ScrollContext.Provider>
-=======
-        [currentYoffset],
-    );
-
-    const messageOpacityA = useMemo(() => {
-        let values = sceneInfo[currentScene].values;
-        let messageA_opacity_in = calcValues(values.messageOpacityA);
-        return messageA_opacity_in;
-    }, [currentYoffset, calcValues]);
+    }, [currentYoffset, sceneInfo, fadeIn]);
 
     return (
         <>
@@ -413,30 +328,30 @@ const Introduce = () => {
                         <div>
                             <Canvas />
                         </div>
-                        <MainMessage messageOpacity={messageOpacityA}>
+                        <MainMessage messageOpacity={messageOpacity[0]}>
                             <p>
                                 코로나로 인해 늘어난
                                 <br /> 배달 주문
                             </p>
                         </MainMessage>
-                        {/* <MainMessage>
+                        <MainMessage messageOpacity={messageOpacity[1]}>
                             <p>
                                 치킨은 점심 때 먹었는데...
                                 <br /> 그럼 저녁엔 뭘 먹지?
                             </p>
                         </MainMessage>
-                        <MainMessage>
+                        <MainMessage messageOpacity={messageOpacity[2]}>
                             <p>
                                 오늘 비가 온다는데
                                 <br /> 파전이나 먹을까?
                             </p>
                         </MainMessage>
-                        <MainMessage>
+                        <MainMessage messageOpacity={messageOpacity[3]}>
                             <p>
                                 이젠 메뉴 고르는 것도
                                 <br /> 일인 지금!
                             </p>
-                        </MainMessage> */}
+                        </MainMessage>{' '}
                     </>
                 )}
             </Scene>
