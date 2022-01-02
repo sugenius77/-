e2d67a09712aac37e6dc54a8aa44d177366cae37
Hongjoonhyung <sesa0802@naver.com>
2022-01-02 17:11:50 +0900

    feat:코드 정리

--- client/src/components/RealTime/Card.jsx
+++ client/src/components/RealTime/Card.jsx
@@ -4,9 +4,9 @@ import { RealTimeContext } from './RealTime';
 
 const Div = styled.div`
     width: 100%;
-    height: 100%;
-    margin-top: 20vh;
+    height: 100vh;
     display: flex;
+    margin: auto;
     bottom: 0;
     direction: row;
     justify-content: space-between;
