7019c35fb82afc2188cd9d3ac766fc8159c9a919
Hongjoonhyung <sesa0802@naver.com>
2022-01-02 14:18:05 +0900

    feat:image 추가

--- client/src/Pages/index.jsx
+++ client/src/Pages/index.jsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 
 import DetailRecommend from '../components/Detail/DetailRecomend';
+import Developer from '../components/Developer/Developer';
 import Introduce from '../components/Introduce/Introduce';
 import RealTime from '../components/RealTime/RealTime';
 
@@ -16,7 +17,9 @@ const Index = () => {
                 </Route>
                 <Route path="/random">랜덤 추천</Route>
                 <Route path="/worldcup">음식 월드컵</Route>
-                <Route path="/developer">개발자 소개</Route>
+                <Route path="/developer">
+                    <Developer />
+                </Route>
                 <Route exact path="/">
                     <Introduce />
                 </Route>
