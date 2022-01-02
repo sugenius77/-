a0959f3209b00ac22d75dd3114ebff3b5cce42c4
Hongjoonhyung <sesa0802@naver.com>
2022-01-02 14:18:05 +0900

    feat:image 추가

---
+++ client/src/components/RealTime/Card.jsx
@@ -0,0 +1,33 @@
+import RecommendCard from './RecommandCard';
+import styled from 'styled-components';
+import { RealTimeContext } from './RealTime';
+
+const Div = styled.div`
+    width: 100%;
+    height: 100%;
+    top: 50vh;
+    display: flex;
+    direction: row;
+    justify-content: space-between;
+`;
+
+const Card = () => {
+    return (
+        <RealTimeContext.Consumer>
+            {({ lotation, recommendHandler }) => (
+                <Div>
+                    <RecommendCard key={'날씨'} title="맑음" path="이미지 경로1" food="치킨" onClick={recommendHandler} />
+                    <RecommendCard key={'요일'} title={lotation.day} path="이미지 경로2" food="중식" onClick={recommendHandler} />
+                    <RecommendCard
+                        key={'시간'}
+                        title={lotation.hours + '시 ' + lotation.minutes + '분'}
+                        path="이미지 경로3"
+                        food="양식"
+                        onClick={recommendHandler}
+                    />
+                </Div>
+            )}
+        </RealTimeContext.Consumer>
+    );
+};
+export default Card;
