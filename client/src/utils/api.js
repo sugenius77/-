43336c879adee1c8738e12e38aab5a15cd9b2ccf
Hongjoonhyung <sesa0802@naver.com>
2022-01-02 14:36:07 +0900

    feat:Header Toggle 시 Static 전환

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,6 @@
-import Weather from './weather.js';
-const apisUrl = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
-const serviceKey = 'y0g%2Fa5rg15oSMXYYVZyErtu%2BUVmJSyn4JXkrl8FM6VKKxwCfvIjopgp1KQMhGJyt7EHFQ6OZv99R%2ByxDNht15Q%3D%3D';
-const api = { Weather };
+import weather from './weather.js';
 
-const weather = new api.Weather(apisUrl, serviceKey);
+const api = { weather };
 
 weather.getPTY();
 
