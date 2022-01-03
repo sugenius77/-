28a10598fe4f28e85ba2011f15009929a9fe5cb9
nyh <dkzndk1100@naver.com>
2022-01-03 16:47:19 +0900

    feat : GET 요청시 - 선택된 월드컵 음식 개수만큼 메뉴이름과 메뉴랭킹, image_url을 반환 - api 설명 수정

--- server/controller/worldcup.py
+++ server/controller/worldcup.py
@@ -1,17 +1,16 @@
 from flask import Blueprint , request, jsonify
 from models.rabbitMenu import rabbitMenu, db
-
+from sqlalchemy.sql.expression import func
 
 worldcup = Blueprint('worldcup', __name__, url_prefix='/worldcup')
 status_code ={'success':200, 'bad_request':400, 'server_error':500}
 
 '''
     GET 요청시
-    figma 4-2의 전체 메뉴 식별자와 메뉴의 랭킹, 이미지 주소 반환
+    전체 메뉴 식별자와 메뉴의 랭킹, 이미지 주소 반환
 
     PUT 요청시
-    figma 4-2의 선택된 메뉴의 레이팅 향상
-
+    선택된 메뉴의 레이팅 향상
     
 '''
 @worldcup.route('/rank',methods=['GET','PUT'])
@@ -69,3 +68,34 @@ def output_rank():
         return jsonify(status_code['bad_request'])
 
 
+'''
+    GET 요청시
+    선택된 월드컵 음식 개수만큼
+    메뉴이름과 메뉴 랭킹, image_url을 반환한다.
+    요청 예시: server주소/start_rank?round=64
+'''
+
+@worldcup.route('/start_rank', methods=['GET'])
+def start_rank():
+    
+    if not request.args:
+        return jsonify(status_code['bad_request'])
+    
+    if request.method == 'GET':
+        parameter_dict = request.args.to_dict()
+        round = parameter_dict['round']
+        
+        ranking_list = rabbitMenu.query\
+            .with_entities(
+                rabbitMenu.menu_name,
+                rabbitMenu.world_ranking,
+                rabbitMenu.image_url
+            ).order_by(func.rand())\
+            .limit(int(round))
+
+        ranking_list =dict((a,[b,c]) for a,b,c in ranking_list)
+
+        return jsonify(ranking_list)
+    
+    else:
+        return jsonify(status_code['bad_request'])
