a6218f803650021e59c14cf7c6c48f032394cffb
YeongHun <dkzndk1499@gmail.com>
2022-01-03 15:02:29 +0900

    feat : refactoring

--- server/app.py
+++ server/app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from models.db_connect import db
 from flask_migrate import Migrate
 from config import config
+<<<<<<< backend
 
 from controller import food_recommend
+=======
+from server.controller import food_recommend
+>>>>>>> feat : refactoring
 from flask_cors import CORS
 from controller import worldcup
 
@@ -34,7 +38,6 @@ def create_app():
     migrate.init_app(app, db)
     
     app.register_blueprint(food_recommend.food)
-    app.register_blueprint(main_view.bp)
     app.register_blueprint(worldcup.worldcup)
 
     app.register_blueprint(worldcup.worldcup)
