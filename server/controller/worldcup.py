b4b499c4fff0e70eabf6adc9186d39bc8e1fb221
nyh <dkzndk1100@naver.com>
2022-01-04 11:54:43 +0900

    feat : api 형태 변경 - 반환 형식 변경

--- server/controller/worldcup.py
+++ server/controller/worldcup.py
@@ -24,7 +24,7 @@ def rank():
                 rabbitMenu.world_ranking,
                 rabbitMenu.image_url
             ).all()
-        ranking_list =dict((a,[b,c]) for a,b,c in ranking_list)
+        ranking_list =dict((a,{"img_url":c,"ranking": b}) for a,b,c in ranking_list)
 
         return jsonify(ranking_list)
 
@@ -93,7 +93,8 @@ def start_rank():
             ).order_by(func.rand())\
             .limit(int(round))
 
-        ranking_list =dict((a,[b,c]) for a,b,c in ranking_list)
+        ranking_list =dict((a,{"img_url":c,
+            "ranking": b}) for a,b,c in ranking_list)
 
         return jsonify(ranking_list)
     
