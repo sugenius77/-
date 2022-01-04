791a995091851142dc75e22770597bb5966ef730
Hongjoonhyung <sesa0802@naver.com>
2022-01-05 05:13:05 +0900

    feat:이미지 파일 추가 및 로딩 타이머 구현

--- client/src/components/RealTime/RealTimeView.jsx
+++ client/src/components/RealTime/RealTimeView.jsx
@@ -4,30 +4,16 @@ import Loading from '../Loading';
 import Recommendation from './Recommendation';
 
 const Div = styled.div`
+    margin-top: 15vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
 `;
-export const RealTimeView = ({ loading, loadingPercent, setLoadingPercent, recommendHandler }) => {
+export const RealTimeView = ({ loading, loadingPercent, recommendHandler, data }) => {
     return (
         <Div>
             {!loading && <Loading loadingPercent={loadingPercent} />}
-            <p>{loadingPercent}</p>
-            <button
-                onClick={() => {
-                    setLoadingPercent((cur) => cur + 25);
-                }}
-            >
-                버튼
-            </button>
-            <button
-                onClick={() => {
-                    setLoadingPercent(0);
-                }}
-            >
-                초기화
-            </button>
-            <Card recommendHandler={recommendHandler} />
+            {loading && <Card recommendHandler={recommendHandler} data={data} />}
         </Div>
     );
 };
