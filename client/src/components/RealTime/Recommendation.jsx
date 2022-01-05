d5227297d60a9a94b4eefa5f41bc1f7ef1aecb5b
Hongjoonhyung <sesa0802@naver.com>
2022-01-05 09:10:18 +0900

    detail 페이지 grid 구현

---
+++ client/src/components/RealTime/Recommendation.jsx
@@ -0,0 +1,47 @@
+import Card from './Card';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    
+    body {
+        color: red;
+    }
+
+    @media screen and (max-width: 600px) {
+        display:flex;
+        flex-direction:column;
+    } 
+
+    @media screen and (max-height: 10)
+}
+`;
+
+const Recommendation = ({ recommendHandler, data }) => {
+    return (
+        <Div>
+            {data.map((info) => (
+                <Card
+                    key={info?.value}
+                    title={info?.value}
+                    path={info?.image_url[0]}
+                    food={info?.kinds_name[0]}
+                    onClick={recommendHandler}
+                />
+            ))}
+            {data.map((info) => (
+                <Card
+                    key={info?.value}
+                    title={info?.value}
+                    path={info?.image_url[1]}
+                    food={info?.kinds_name[1]}
+                    onClick={recommendHandler}
+                />
+            ))}
+
+            {/*<RecommendCard key="weather" title={data.weather.weather} path="이미지 경로3" food="양식" onClick={recommendHandler} /> */}
+        </Div>
+    );
+};
+export default Recommendation;
