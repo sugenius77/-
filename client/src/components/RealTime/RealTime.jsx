2fc5a39388bb06db23934d743ddf0e824d8506b2
Hongjoonhyung <sesa0802@naver.com>
2022-01-14 19:27:05 +0900

    feat: geolocation http 문제로 인해 수정

--- client/src/components/RealTime/RealTime.jsx
+++ client/src/components/RealTime/RealTime.jsx
@@ -2,18 +2,18 @@ import { createContext, useState, useMemo, useEffect, useCallback } from 'react'
 import { useHistory } from 'react-router-dom';
 import RealTimeView from './RealTimeView';
 import api from '../../utils/api/api';
-import useCurrentLocation from '../../utils/hooks/useCurrentLocation';
 export const RealTimeContext = createContext();
 
-const geolocationOptions = {
-    enableHighAccuracy: true,
-    timeout: 1000 * 60 * 1,
-    maximumAge: 1000 * 3600 * 24,
-};
+//import useCurrentLocation from '../../utils/hooks/useCurrentLocation';
+// const geolocationOptions = {
+//     enableHighAccuracy: true,
+//     timeout: 1000 * 60 * 1,
+//     maximumAge: 1000 * 3600 * 24,
+// };
 
 const RealTime = () => {
     const [loadingPercent, setLoadingPercent] = useState(0);
-    const { location: currentLocation, error: currentError } = useCurrentLocation(geolocationOptions);
+    // const { location: currentLocation, error: currentError } = useCurrentLocation(geolocationOptions);
     const [data, setData] = useState([]);
     const history = useHistory();
     const loading = useMemo(() => (loadingPercent >= 125 ? true : false), [loadingPercent]);
@@ -24,8 +24,10 @@ const RealTime = () => {
     };
 
     const getRealTime = useCallback(async () => {
-        let nx = parseInt(currentLocation.latitude);
-        let ny = parseInt(currentLocation.longitude);
+        // let nx = parseInt(currentLocation.latitude);
+        // let ny = parseInt(currentLocation.longitude);
+        let nx = 37;
+        let ny = 127;
         const res = await api.realtime.getRealTime(nx, ny);
         if (res.data !== '500 server error') {
             console.log(res);
@@ -34,8 +36,7 @@ const RealTime = () => {
                 return newData;
             });
             setTimeout(() => {
-                setLoadingPercent((cur) => cur + 25);
-                setLoadingPercent((cur) => cur + 25);
+                setLoadingPercent((cur) => cur + 50);
             }, 2000);
             //eslint-disable-next-line
         } else {
@@ -45,41 +46,33 @@ const RealTime = () => {
                 return newData;
             });
         }
-    }, [currentLocation]);
+    }, []);
 
-    useEffect(() => {
-        if (!currentLocation) {
-            console.log(currentError);
-            return;
-        } else if (data.length <= 1) {
-            getRealTime();
-        }
-        //eslint-disable-next-line
-    }, [getRealTime]);
+    // useEffect(() => {
+    //     if (!currentLocation) {
+    //         console.log(currentError);
+    //         return;
+    //     } else if (data.length <= 1) {
+    //         getRealTime();
+    //     }
+    //     //eslint-disable-next-line
+    // }, [getRealTime]);
 
     useEffect(() => {
         if (data.length > 0) {
             setTimeout(() => {
-                setLoadingPercent((cur) => cur + 25);
+                setLoadingPercent((cur) => cur + 50);
             }, 4000);
             setTimeout(() => {
                 setLoadingPercent((cur) => cur + 25);
             }, 5000);
-            setTimeout(() => {
-                setLoadingPercent((cur) => cur + 25);
-            }, 1000);
         }
     }, [data]);
 
     useEffect(() => {
-        if (!currentLocation) {
-            console.log(currentError);
-            return;
-        } else {
-            getRealTime(currentLocation.latitude, currentLocation.longitude);
-        }
+        getRealTime();
         //eslint-disable-next-line
-    }, []);
+    }, [getRealTime]);
 
     return (
         <RealTimeView
